204ce657ec5c3f291a30ec750ce0b9f68efa0426
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-11-21 05:47:53 -0400

    Ignorar mayores a 1000

--- CalculadoraCadenas-main/src/calculadora.test.js
+++ CalculadoraCadenas-main/src/calculadora.test.js
@@ -20,9 +20,13 @@ describe("Sumar digitos", () => {
   it("Si la cadena tiene varios numeros y uno de ellos tiene dos o mas digitos, convertirlos a entero y retonar la suma", () => {
     expect(calcularSumaCadena("22,2,3,6")).toEqual(33);
   });
-  
+
   it("La cadena tambien puede separarse por guion", () => {
     expect(calcularSumaCadena("22,2-3-13")).toEqual(40);
+  });
+
+  it("Numeros mayores a 1000 se ignoran", () => {
+    expect(calcularSumaCadena("10001,2-3-13")).toEqual(18);
   });
 
 
